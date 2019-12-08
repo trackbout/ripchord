6b13bb3a24f19426a4e4ecfc738d3c08fc839a3f
Fred Guest <fredguest@mac.com>
2019-12-07 21:06:18 -0800

    add header map configs

--- Source/Components/MainComponent.h
+++ Source/Components/MainComponent.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "JuceHeader.h"
+#include "Constants.h"
 
 //==============================================================================
 class MainComponent : public Component
