17e05bb8e263b95553264a285b49cb859381dd82
Fred Guest <fredguest@mac.com>
2019-12-19 20:12:15 -0800

    delete default processBlock

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -111,32 +111,6 @@ bool RipchordPluginProcessor::isBusesLayoutSupported (const BusesLayout& inLayou
 
 void RipchordPluginProcessor::processBlock (AudioBuffer<float>& inAudioBuffer, MidiBuffer& inMidiBuffer)
 {
-    ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
-
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto index = totalNumInputChannels; index < totalNumOutputChannels; ++index)
-    {
-        inAudioBuffer.clear (index, 0, inAudioBuffer.getNumSamples());
-    }
-
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-    // auto* channelData = buffer.getWritePointer (channel);
-    // ..do something to the data...
-    }
 }
 
 //==============================================================================
