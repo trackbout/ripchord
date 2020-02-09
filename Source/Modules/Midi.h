b7dd05a8ab1bfe2298c3c4ee0c3598bb58a17550
Fred Guest <fredguest@mac.com>
2020-02-08 21:30:11 -0800

    add NoteEvent

--- Source/Modules/Midi.h
+++ Source/Modules/Midi.h
@@ -7,3 +7,30 @@ struct Origin
 {
     juce::Array<int> triggers;
 };
+
+struct NoteEvent {
+    int channel;
+    int sampleNumber;
+    float velocity;
+    int inputNote;
+    int outputNote;
+    bool isNoteOn;
+
+    NoteEvent
+    (
+        int& inChannel,
+        int& inSampleNumber,
+        float& inVelocity,
+        int& inInputNote,
+        int& inOutputNote,
+        bool& inIsNoteOn
+    )
+    :   channel (inChannel),
+        sampleNumber (inSampleNumber),
+        velocity (inVelocity),
+        inputNote (inInputNote),
+        outputNote (inOutputNote),
+        isNoteOn (inIsNoteOn)
+    {
+    }
+};
