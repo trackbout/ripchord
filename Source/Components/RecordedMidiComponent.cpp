4b46e5b2282bb4f1e0942999826b323e46eefe3f
Fred Guest <fredguest@mac.com>
2020-11-09 10:42:36 -0800

    perform drag and drop

--- Source/Components/RecordedMidiComponent.cpp
+++ Source/Components/RecordedMidiComponent.cpp
@@ -22,7 +22,25 @@ void RecordedMidiComponent::resized()
 //==============================================================================
 void RecordedMidiComponent::mouseDown (const MouseEvent& inEvent)
 {
-    DBG ("woot");
+    MidiMessageSequence sequence;
+    sequence.addEvent (MidiMessage::noteOn (1, 54, 0.8f), 0.0f);
+    sequence.addEvent (MidiMessage::noteOn (1, 59, 0.8f), 0.0f);
+    sequence.addEvent (MidiMessage::noteOn (1, 62, 0.8f), 0.0f);
+    sequence.addEvent (MidiMessage::noteOff (1, 54), TICKS_PER_QUARTER_NOTE * 4.0f);
+    sequence.addEvent (MidiMessage::noteOff (1, 59), TICKS_PER_QUARTER_NOTE * 4.0f);
+    sequence.addEvent (MidiMessage::noteOff (1, 62), TICKS_PER_QUARTER_NOTE * 4.0f);
+
+    MidiFile midiFile;
+    midiFile.setTicksPerQuarterNote (TICKS_PER_QUARTER_NOTE);
+    midiFile.addTrack (sequence);
+
+    File temp = COMPANY_FOLDER.getChildFile ("ripchord.mid");
+
+    if (auto stream = std::unique_ptr<FileOutputStream> (temp.createOutputStream()))
+    {
+        midiFile.writeTo (*stream, 0);
+        performExternalDragDropOfFiles ({ temp.getFullPathName() }, false, nullptr, [=](void){ temp.deleteFile(); });
+    }
 }
 
 //==============================================================================
